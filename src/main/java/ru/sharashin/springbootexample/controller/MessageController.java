2ce2684272c6248aa073bbc73926eaf82e4c772c
mike <trems95@gmail.com>
2019-06-20 21:13:31 +0500

    init commit

---
+++ src/main/java/ru/sharashin/springbootexample/controller/MessageController.java
@@ -0,0 +1,65 @@
+package ru.sharashin.springbootexample.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import ru.sharashin.springbootexample.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+    private int messageCounter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<>() {{
+       add(new HashMap<>() {{ put("id", "1"); put("text", "1st message"); }});
+       add(new HashMap<>() {{ put("id", "2"); put("text", "2nd message"); }});
+       add(new HashMap<>() {{ put("id", "3"); put("text", "3rd message"); }});
+    }};
+
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(messageCounter++));
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+
+        messages.remove(message);
+    }
+}
